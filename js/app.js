ff57fec40aca72b586ea1ac224c8712dcd1a7f9d
rafalmusial <musial.rafal@gmail.com>
2017-03-02 13:51:32 +0100

    Rozpoczeto prace z JavaScript na stronie

---
+++ js/app.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+// Zadanie 1
+  var oFirmie = document.querySelector('#firma');
+  var lista =document.querySelector('#lista');
+
+  oFirmie.addEventListener("click", function(event) {
+    if (lista.style.display == "block") {
+      lista.style.display == "none"
+    }
+
+
+
+  })
+
+})
