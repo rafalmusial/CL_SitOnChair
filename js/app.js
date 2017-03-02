d5bc7781cd0513c96dab86be6f2339f673307b16
rafalmusial <musial.rafal@gmail.com>
2017-03-02 15:46:03 +0100

    Rozwizano zadanie domowe 1 i 2 z JavaScript

--- js/app.js
+++ js/app.js
@@ -1,15 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
 // Zadanie 1
-  var oFirmie = document.querySelector('#firma');
-  var lista =document.querySelector('#lista');
+var menuButton = document.querySelector('.menu a');
+var subMenuButton = document.querySelector('ul.menu li ul');
 
-  oFirmie.addEventListener("click", function(event) {
-    if (lista.style.display == "block") {
-      lista.style.display == "none"
-    }
+console.log(menuButton);
+console.log(subMenuButton);
+menuButton.addEventListener('mouseover', function() {
+  subMenuButton.style.display = "inline-block";
+});
+menuButton.addEventListener('mouseout', function() {
+  subMenuButton.style.display = "none";
+});
 
+//Zadanie 2
+var product1 = document.querySelector('div#box1');
+var product2 = document.querySelector('div#box2');
 
-
-  })
-
+product1.addEventListener('mouseover', function() {
+    product1.firstElementChild.classList.add('showTitle');
+});
+product1.addEventListener('mouseout', function() {
+    product1.firstElementChild.classList.remove('showTitle');
+});
+product2.addEventListener('mouseover', function() {
+    product2.firstElementChild.classList.add('showTitle');
+});
+product2.addEventListener('mouseout', function() {
+    product2.firstElementChild.classList.remove('showTitle');
+});
 })
