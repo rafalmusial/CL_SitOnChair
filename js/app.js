86e55a877789b7a7ca025977d8f467751c133250
rafalmusial <musial.rafal@gmail.com>
2017-03-03 13:32:16 +0100

    Rozwiazano zadanie domowe 3 z JavaScript

--- js/app.js
+++ js/app.js
@@ -1,33 +1,52 @@
 document.addEventListener('DOMContentLoaded', function () {
-// Zadanie 1
-var menuButton = document.querySelector('.menu a');
-var subMenuButton = document.querySelector('ul.menu li ul');
+  // Zadanie 1
+  var menuButton = document.querySelector('.menu a');
+  var subMenuButton = document.querySelector('ul.menu li ul');
 
-console.log(menuButton);
-console.log(subMenuButton);
-menuButton.addEventListener('mouseover', function(event) {
-  subMenuButton.style.display = "inline-block";
-});
-menuButton.addEventListener('mouseout', function(event) {
-  subMenuButton.style.display = "none";
-});
+  menuButton.addEventListener('mouseover', function(event) {
+    subMenuButton.style.display = "inline-block";
+  });
+  menuButton.addEventListener('mouseout', function(event) {
+    subMenuButton.style.display = "none";
+  });
 
-//Zadanie 2
-var product1 = document.querySelector('div#box1');
-var product2 = document.querySelector('div#box2');
+  //Zadanie 2
+  var product1 = document.querySelector('div#box1');
+  var product2 = document.querySelector('div#box2');
 
-product1.addEventListener('mouseover', function() {
-    product1.firstElementChild.classList.add('showTitle');
-});
-product1.addEventListener('mouseout', function() {
-    product1.firstElementChild.classList.remove('showTitle');
-});
-product2.addEventListener('mouseover', function() {
-    product2.firstElementChild.classList.add('showTitle');
-});
-product2.addEventListener('mouseout', function() {
-    product2.firstElementChild.classList.remove('showTitle');
-});
-//Zdaanie 3
+  product1.addEventListener('mouseover', function() {
+      product1.firstElementChild.classList.add('showTitle');
+  });
+  product1.addEventListener('mouseout', function() {
+      product1.firstElementChild.classList.remove('showTitle');
+  });
+  product2.addEventListener('mouseover', function() {
+      product2.firstElementChild.classList.add('showTitle');
+  });
+  product2.addEventListener('mouseout', function() {
+      product2.firstElementChild.classList.remove('showTitle');
+  });
+  //Zdaanie 3
+  var buttonLeft = document.querySelector('.arrow-left');
+  var buttonRight = document.querySelector('.arrow-right');
+  var images = document.querySelectorAll('section.slider-wrap ul li img');
+  var index = 0;
 
+  buttonRight.addEventListener('click', function(event){
+    images[index].classList.add('showTitle');
+    index++;
+    if (index > images.length-1) {
+      index = 0;
+    }
+    images[index].classList.remove('showTitle');
+  })
+
+  buttonLeft.addEventListener("click", function(event) {
+    images[index].classList.add('showTitle');
+    index--;
+    if (index < 0) {
+      index = images.length-1;
+    }
+    images[index].classList.remove('showTitle');
+  })
 })
